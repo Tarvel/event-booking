58f9152a19da9d6b90b3f6f32e12fa992fc59805
tarvel <taiyebabatundejames@gmail.com>
2025-08-31 14:30:48 +0100

    updated home views and templates

--- events/views.py
+++ events/views.py
@@ -16,39 +16,46 @@ def home(request):
     method = request.method
     page = request.GET.get("page", 1)
 
-    category = request.POST.get("category", "all")
-
-    s_date = request.COOKIES.get("tempWord") or "all"
+    category = request.COOKIES.get("tempCategory") or "all"
+    date = request.COOKIES.get("tempDate") or "all"
 
     filters = Q()
-    compared_date = datetime.now()
+    compared_date = datetime.now().date()
 
-    if category:
+    if category and category != "all":
         filters &= Q(category=category)
 
-    if s_date == "today":
+    if date == "today":
         filters &= Q(start_date=compared_date)
 
-    elif s_date == "week":
-        filters &= Q(start_date__week=compared_date.isocalendar()[1])
+    elif date == "week":
+        filters &= Q(start_date__week=(compared_date.isocalendar()[1]))
         filters &= Q(start_date__year=compared_date.year)
 
-    elif s_date == "month":
+    elif date == "month":
         filters &= Q(start_date__month=compared_date.month)
 
     events = (
-        Events.objects.all()
-        if category == "all" and s_date == "all"
-        else Events.objects.filter(filters)
+        Events.objects.all().order_by("-created_at")
+        if category == "all" and date == "all"
+        else Events.objects.filter(filters).order_by("-created_at")
     )
-    print(f"this is {s_date}")
+    print(
+        f"this is {date}\n compared week {compared_date.isocalendar()[1]}\n today {compared_date}"
+    )
+    print("compared_date:", compared_date)
+    print("filters:", filters)
+    print(
+        "events in DB:", list(Events.objects.values_list("start_date__week", flat=True))
+    )
+
     paginator = Paginator(events, 8)
     events_page = paginator.get_page(page)
 
     context = {
         "events": events_page,
         "active_category": category,
-        "active_date": s_date,
+        "active_date": date,
     }
 
     if request.htmx:
