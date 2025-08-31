cfaf3cd20501d698d3a68ec6d8ee6d32d5fba2bc
Taiye James Babatunde <76518569+Tarvel@users.noreply.github.com>
2025-08-31 16:29:53 +0100

    Feature/events home (#1)
    * changed navigation form to regular post not htmx
    
    * updated date filter by adding selected value to cookie
    
    * updated home views and templates

--- events/views.py
+++ events/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.contrib.auth import login, logout, authenticate
 import uuid
+from datetime import datetime
 from .models import Events
 from django.contrib.auth import get_user_model
 
@@ -11,17 +13,40 @@ User = get_user_model()
 
 
 def home(request):
+    method = request.method
     page = request.GET.get("page", 1)
-    category = (
-        request.POST.get("category", "all")
-        if request.method == "POST"
-        else request.GET.get("category", "all")
-    )
+
+    category = request.COOKIES.get("tempCategory") or "all"
+    date = request.COOKIES.get("tempDate") or "all"
+
+    filters = Q()
+    compared_date = datetime.now().date()
+
+    if category and category != "all":
+        filters &= Q(category=category)
+
+    if date == "today":
+        filters &= Q(start_date=compared_date)
+
+    elif date == "week":
+        filters &= Q(start_date__week=(compared_date.isocalendar()[1]))
+        filters &= Q(start_date__year=compared_date.year)
+
+    elif date == "month":
+        filters &= Q(start_date__month=compared_date.month)
 
     events = (
         Events.objects.all().order_by("-created_at")
-        if category == "all"
-        else Events.objects.filter(category=category).order_by("-created_at")
+        if category == "all" and date == "all"
+        else Events.objects.filter(filters).order_by("-created_at")
+    )
+    print(
+        f"this is {date}\n compared week {compared_date.isocalendar()[1]}\n today {compared_date}"
+    )
+    print("compared_date:", compared_date)
+    print("filters:", filters)
+    print(
+        "events in DB:", list(Events.objects.values_list("start_date__week", flat=True))
     )
 
     paginator = Paginator(events, 8)
@@ -30,11 +55,11 @@ def home(request):
     context = {
         "events": events_page,
         "active_category": category,
+        "active_date": date,
     }
 
     if request.htmx:
         return render(request, "events/partials/event_list.html", context)
-
     return render(request, "events/home.html", context)
 
 
