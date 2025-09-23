b529c9efdd796d4f7b50455ed0222a9584415bc4
tarvel <taiyebabatundejames@gmail.com>
2025-09-23 18:28:30 +0100

    omo another final push i don tire

--- config/settings.py
+++ config/settings.py
@@ -137,6 +137,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'theme', 'static'),  # adjust to your Tailwind app
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
