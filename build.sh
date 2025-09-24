6fab8854304773af3ce485b01df09da8768d8afd
tarvel <taiyebabatundejames@gmail.com>
2025-09-24 18:02:44 +0100

    added dummy data

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ python manage.py migrate --noinput
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+echo "loading fixture data"
+python manage.py loaddata events.json
 
 echo "Starting server..."
 gunicorn --bind 0.0.0.0:8000 --workers 3 config.wsgi:application
